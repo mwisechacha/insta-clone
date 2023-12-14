20f32cf5c700d75b8689ec4ffba4204ab35fecf8
mwisechacha <angieschacha@gmail.com>
2023-12-14 13:31:52 +0300

    adding models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
 from .validators import validate_file_size
@@ -8,6 +9,7 @@ class Post(models.Model):
     caption = models.CharField(max_length=1000)
     posted_at = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveSmallIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # def __str__(self) -> str:
     #     return super().__str__()
@@ -22,3 +24,25 @@ class PostImage(models.Model):
     
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Follow(models.Model):
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+class Story(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='story_following')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.user
+    
+    class Meta:
+        ordering = ['created_at']
